a6fa0a46d5a1cf4e3cc0ba474c59835efdc83a48
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-09-09 19:57:40 +0530

    Update alives.py

--- userbot/plugins/alives.py
+++ userbot/plugins/alives.py
@@ -6,7 +6,7 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/6f8d9001de13b3f8e573a.jpg"
+PM_IMG = "https://telegra.ph/file/59e521721f043e33f7c0a.jpg"
 pm_caption = "**Fᵣᵢdₐy ᵢₛ ₒₙₗᵢₙₑ**\n"
 
 pm_caption += f"**M̴y̴ ̴B̴o̴s̴s̴**            : {DEFAULTUSER}\n"
